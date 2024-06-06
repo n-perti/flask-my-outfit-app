a5c32dfee73df9b4ef1c249055489e57344396f0
Nico <nicolaspertierra@Nicolass-MacBook-Pro.local>
2024-06-06 10:15:01 +0200

    feat(Setup): Setup
    Merge code and trained model to render cloud hosting

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from PIL import Image
+from ultralytics import YOLO
+from collections import Counter
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+model_detection = YOLO("MyOutfitApp_trained_best.pt")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        image_file = request.files['file']
+        image = Image.open(image_file)
+
+
+        results = model_detection(image)
+
+
+        detected_items = [model_detection.names[int(box.cls)] for box in results[0].boxes]
+
+
+        label_counts = Counter(detected_items)
+
+        formatted_detected_items = ', '.join([f"{count} {label}" for label, count in label_counts.items()])
+
+        return jsonify({"detected_items": formatted_detected_items})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
